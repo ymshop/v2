2e7a69f7e0974a240b9e6baa486f4c0f42e36e9a
Muhammad Azrial <azrialmons@gmail.com>
2020-08-07 15:00:18 +0700

    create content area

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="w-screen h-screen bg-gray-300">
+    <div class="container flex justify-center w-full h-full">
+      <div class="w-full max-w-sm max-h-full min-h-full overflow-y-auto bg-white shadow-xl">
+        <Nuxt />
+      </div>
+    </div>
   </div>
 </template>
